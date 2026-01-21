d88ea1becb83ff88e4d6c74dbc4015039a6d2259
wtalvaro <wagneralvaro80@gmail.com>
2026-01-21 08:19:01 +0000

    Modify `AppDbContext.cs` and update associated build artifacts, including creating `IndustrialMonitor.sourcelink.json`.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,11 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IndustrialMonitor.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
-
-    public DbSet<Maquina> Maquinas { get; set; }
+    public DbSet<Maquina> Maquinas { get; set; } = default!;
 }
